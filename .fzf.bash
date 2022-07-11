ad18cbf4042357cd9b2fc9a37ca4efd231c3c5c6
Ludmila Bertier <8851473+ludbertier@users.noreply.github.com>
2022-07-11 21:01:38 +0200

    Update .fzf.bash

--- .fzf.bash
+++ .fzf.bash
@@ -16,3 +16,29 @@ fi
 # Key bindings
 # ------------
 source "/home/qw40wz/.fzf/shell/key-bindings.bash"
+
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+$ which fzf
+/opt/homebrew/bin/fzf
+
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+-------------------------------------------------------------------
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/bin/fzf* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/bin/fzf"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.30.0/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/Cellar/fzf/0.30.0/shell/key-bindings.zsh"
